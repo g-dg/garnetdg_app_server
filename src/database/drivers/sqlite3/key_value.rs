c9e4ad1121600553d5356b3a49ea980eea83b772
Garnet DeGelder <garnet@garnetdg.ca>
2024-01-26 18:41:40 -0500

    WIP: key-value set

--- src/database/drivers/sqlite3/key_value.rs
+++ src/database/drivers/sqlite3/key_value.rs
@@ -42,16 +42,13 @@ impl SQLite3Connection {
         fn get_value_recursive(
             conn: &PooledConnection<SqliteConnectionManager>,
             current_id: Option<i64>,
-            key: &[&str],
             table_prefix: &str,
+            key: &[&str],
         ) -> Option<String> {
             if key.len() > 0 {
                 // get child id
                 let mut stmt = conn
-                    .prepare_cached(&format!(
-                        "SELECT \"id\" FROM \"{}key_values\" WHERE \"id\" = :id AND \"key\" = :key;",
-                        table_prefix
-                    ))
+                    .prepare_cached(&format!("SELECT \"id\" FROM \"{}key_values\" WHERE \"id\" = :id AND \"key\" = :key;", table_prefix))
                     .expect("Error occurred while preparing SQL statement");
                 let new_id: Option<i64> = stmt
                     .query_row(named_params! {":id": current_id, ":key": key[0]}, |row| {
@@ -61,7 +58,7 @@ impl SQLite3Connection {
                     .expect("Error occurred while querying database");
                 // if returned the child id, then recurse
                 if let Some(new_id) = new_id {
-                    get_value_recursive(conn, Some(new_id), key, table_prefix)
+                    get_value_recursive(conn, Some(new_id), table_prefix, &key[1..])
                 } else {
                     None
                 }
@@ -79,7 +76,7 @@ impl SQLite3Connection {
             }
         }
 
-        get_value_recursive(&conn, None, key, &table_prefix)
+        get_value_recursive(&conn, None, &table_prefix, key)
     }
 
     pub fn key_value_set(
@@ -94,6 +91,62 @@ impl SQLite3Connection {
             .get()
             .expect("Could not get database connection from connection pool");
 
-        todo!()
+        let table_prefix = Self::get_table_prefix(table_prefix, Some(store_name));
+
+        fn set_value_recursive(
+            conn: &PooledConnection<SqliteConnectionManager>,
+            current_id: Option<i64>,
+            table_prefix: &str,
+            key: &[&str],
+            value: &str,
+        ) {
+            if key.len() > 0 {
+                // navigating towards path
+                // check if next path record exists
+                let mut stmt = conn
+                    .prepare_cached(&format!("SELECT \"id\" FROM \"{}key_values\" WHERE \"id\" = :id AND \"key\" = :key;", table_prefix))
+                    .expect("Error occurred while preparing SQL statement");
+                let new_id: Option<i64> = stmt
+                    .query_row(named_params! {":id": current_id, ":key": key[0]}, |row| {
+                        row.get(0)
+                    })
+                    .optional()
+                    .expect("Error occurred while querying the database");
+                if let Some(new_id) = new_id {
+                    // record exists, recurse
+                    set_value_recursive(conn, Some(new_id), table_prefix, &key[1..], value)
+                } else {
+                    // record doesn't exist, recurse anyways
+                    //TODO: make so it doesn't have to be created then set in separate operations
+                }
+            } else {
+                // at end of path
+                // check if current path record exists
+            }
+
+            /*
+               - if next key
+                   - if exists
+                       - recurse
+                   - else
+                       - create and recurse
+               - else
+                   - if exists
+                       - set
+                   - else
+                       - create
+                
+                if setting value of current key id
+                    check if exists
+                    if record of current key id and exists
+                        update record
+                    else
+                        insert record
+            */
+
+            // Need to find way to store value at the root of the store
+        }
+
+        set_value_recursive(&conn, None, &table_prefix, key, value)
     }
 }
