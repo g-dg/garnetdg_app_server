9c77237c5955fccf8dd80314389f5aca28b00de7
Garnet DeGelder <garnet@garnetdg.ca>
2024-02-26 21:08:14 -0500

    Make key-value tree in database use 2 separate tables

--- database_schemas.sql
+++ database_schemas.sql
@@ -1,31 +1,38 @@
 -- Key-value storage
 
-CREATE TABLE IF NOT EXISTS "key_values" (
+CREATE TABLE IF NOT EXISTS "key_value_tree" (
 	"id" INTEGER PRIMARY KEY NOT NULL,
-	"parent_id" INTEGER REFERENCES "key_values" ("id"),
-	"key" TEXT NOT NULL,
+	"parent_id" INTEGER REFERENCES "key_value_tree" ("id"),
+	"key" TEXT NOT NULL
+);
+CREATE UNIQUE INDEX IF NOT EXISTS "index_key_value_tree__ifnull_parent_id__key" ON "key_value_tree" (IFNULL("parent_id", 0), "key");
+CREATE INDEX IF NOT EXISTS "index_key_value_tree__parent_id__key" ON "key_value_tree" ("parent_id", "key");
+
+CREATE TABLE IF NOT EXISTS "key_value" (
+	"id" INTEGER PRIMARY KEY NOT NULL,
+	"tree_node_id" INTEGER REFERENCES "key_value_tree" ("id"),
 	"value" TEXT
 );
-CREATE UNIQUE INDEX IF NOT EXISTS "index_key_values__ifnull_parent_id__key" ON "key_values" (IFNULL("parent_id", 0), "key");
-CREATE INDEX IF NOT EXISTS "index_key_values__parent_id__key" ON "key_values" ("parent_id", "key");
+CREATE UNIQUE INDEX IF NOT EXISTS "index_key_value__ifnull_tree_node_id" ON "key_value" (IFNULL("tree_node_id", 0));
+CREATE INDEX IF NOT EXISTS "index_key_value__tree_node_id" ON "key_value" ("tree_node_id");
 
 
 -- Message queue storage
 
-CREATE TABLE IF NOT EXISTS "message_paths" (
+CREATE TABLE IF NOT EXISTS "message_tree" (
 	"id" INTEGER PRIMARY KEY NOT NULL,
-	"parent_id" INTEGER REFERENCES "message_paths",
-	"name" TEXT NOT NULL,
+	"parent_id" INTEGER REFERENCES "message_tree",
+	"name" TEXT NOT NULL
 );
-CREATE UNIQUE INDEX IF NOT EXISTS "index_message_paths__ifnull_parent_id__name" on "message_paths" (IFNULL("parent_id", 0), "name");
-CREATE INDEX IF NOT EXISTS "index_message_paths__parent_id__name" on "message_paths" ("parent_id", "name");
+CREATE UNIQUE INDEX IF NOT EXISTS "index_message_tree__ifnull_parent_id__name" on "message_tree" (IFNULL("parent_id", 0), "name");
+CREATE INDEX IF NOT EXISTS "index_message_tree__parent_id__name" on "message_tree" ("parent_id", "name");
 
 CREATE TABLE IF NOT EXISTS "messages" (
 	"id" INTEGER PRIMARY KEY NOT NULL,
-	"path_id" INTEGER NOT NULL REFERENCES "message_paths",
+	"path_id" INTEGER NOT NULL REFERENCES "message_tree",
 	"message_id" TEXT NOT NULL UNIQUE,
 	"timestamp" TEXT NOT NULL,
-	"message" TEXT NOT NULL,
+	"message" TEXT NOT NULL
 );
 CREATE INDEX IF NOT EXISTS "_index_messages__path_id" ON "messages" ("path_id");
 CREATE INDEX IF NOT EXISTS "_index_messages__timestamp" ON "messages" ("timestamp");
@@ -56,5 +63,5 @@ CREATE TABLE IF NOT EXISTS "sessions" (
 	"id" INTEGER PRIMARY KEY NOT NULL,
 	"token" TEXT NOT NULL UNIQUE,
 	"user_id" INTEGER NOT NULL REFERENCES "_users" ("id"),
-	"timestamp" TEXT NOT NULL,
+	"timestamp" TEXT NOT NULL
 );
