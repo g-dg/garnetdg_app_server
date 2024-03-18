a6c54c4352487f75ba463758c53e9cfbf7026e8a
Garnet DeGelder <garnet@garnetdg.ca>
2024-03-18 11:25:14 -0400

    Update database schema documentation

--- database_schemas.sql
+++ database_schemas.sql
@@ -1,42 +1,22 @@
--- Key-value storage
+-- Datastore storage
 
-CREATE TABLE IF NOT EXISTS "key_value_tree" (
+CREATE TABLE IF NOT EXISTS "datastore_tree" (
 	"id" INTEGER PRIMARY KEY NOT NULL,
-	"parent_id" INTEGER REFERENCES "key_value_tree" ("id"),
+	"parent_id" INTEGER REFERENCES "datastore_tree" ("id"),
 	"key" TEXT NOT NULL
 );
-CREATE UNIQUE INDEX IF NOT EXISTS "index_key_value_tree__ifnull_parent_id__key" ON "key_value_tree" (IFNULL("parent_id", 0), "key");
-CREATE INDEX IF NOT EXISTS "index_key_value_tree__parent_id__key" ON "key_value_tree" ("parent_id", "key");
+CREATE UNIQUE INDEX IF NOT EXISTS "index_datastore_tree__ifnull_parent_id__key" ON "datastore_tree" (IFNULL("parent_id", 0), "key");
+CREATE INDEX IF NOT EXISTS "index_datastore_tree__parent_id__key" ON "datastore_tree" ("parent_id", "key");
 
-CREATE TABLE IF NOT EXISTS "key_value" (
+CREATE TABLE IF NOT EXISTS "datastore_values" (
 	"id" INTEGER PRIMARY KEY NOT NULL,
-	"tree_node_id" INTEGER REFERENCES "key_value_tree" ("id"),
+	"tree_node_id" INTEGER REFERENCES "datastore_tree",
+	"change_id" TEXT NOT NULL UNIQUE,
+	"timestamp" TEXT NOT NULL,
 	"value" TEXT
 );
-CREATE UNIQUE INDEX IF NOT EXISTS "index_key_value__ifnull_tree_node_id" ON "key_value" (IFNULL("tree_node_id", 0));
-CREATE INDEX IF NOT EXISTS "index_key_value__tree_node_id" ON "key_value" ("tree_node_id");
-
-
--- Message queue storage
-
-CREATE TABLE IF NOT EXISTS "message_tree" (
-	"id" INTEGER PRIMARY KEY NOT NULL,
-	"parent_id" INTEGER REFERENCES "message_tree",
-	"name" TEXT NOT NULL
-);
-CREATE UNIQUE INDEX IF NOT EXISTS "index_message_tree__ifnull_parent_id__name" on "message_tree" (IFNULL("parent_id", 0), "name");
-CREATE INDEX IF NOT EXISTS "index_message_tree__parent_id__name" on "message_tree" ("parent_id", "name");
-
-CREATE TABLE IF NOT EXISTS "messages" (
-	"id" INTEGER PRIMARY KEY NOT NULL,
-	"path_id" INTEGER NOT NULL REFERENCES "message_tree",
-	"message_id" TEXT NOT NULL UNIQUE,
-	"timestamp" TEXT NOT NULL,
-	"message" TEXT NOT NULL
-);
-CREATE INDEX IF NOT EXISTS "_index_messages__path_id" ON "messages" ("path_id");
-CREATE INDEX IF NOT EXISTS "_index_messages__timestamp" ON "messages" ("timestamp");
-
+CREATE INDEX IF NOT EXISTS "index_datastore_values__tree_node_id" ON "datastore_values" ("tree_node_id");
+CREATE INDEX IF NOT EXISTS "index_datastore_values__timestamp" ON "datastore_values" ("timestamp");
 
 -- Authentication storage
 
