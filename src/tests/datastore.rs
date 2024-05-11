fd3ef649d91001b68f961b52026b79130c3c98a5
Garnet DeGelder <garnet@garnetdg.ca>
2024-05-11 15:40:52 -0400

    Add datastore basic get & set test

--- src/tests/datastore.rs
+++ src/tests/datastore.rs
@@ -1,12 +1,12 @@
-use crate::{config::DataStoreConfig, datastore::DataStore};
+use crate::{config::DatastoreConfig, datastore::DataStore};
 
 #[tokio::test]
 async fn ping() {
     let datastore: DataStore<String> = DataStore::new(
         "test",
-        DataStoreConfig {
+        DatastoreConfig {
             database_schema: None,
-            keep_history: Some(true),
+            keep_history: true,
             history_max_age: Some(3600),
             history_max_entries: Some(1000),
         },
@@ -14,10 +14,37 @@ async fn ping() {
     )
     .await;
 
-	datastore.ping().await;
+    datastore.ping().await;
 
-	let datastore2 = datastore.clone();
+    let datastore2 = datastore.clone();
 
-	datastore.ping().await;
-	datastore2.ping().await;
+    datastore.ping().await;
+    datastore2.ping().await;
+}
+
+#[tokio::test]
+async fn basic_get_set() {
+    let datastore: DataStore<String> = DataStore::new(
+        "test",
+        DatastoreConfig {
+            database_schema: None,
+            keep_history: false,
+            history_max_age: None,
+            history_max_entries: None,
+        },
+        None,
+    )
+    .await;
+
+    datastore.set(&[], String::from("test1")).await;
+    assert_eq!(
+        datastore.get_current(&[]).await.value,
+        Some(String::from("test1"))
+    );
+
+    datastore.set(&[], String::from("test2")).await;
+    assert_eq!(
+        datastore.get_current(&[]).await.value,
+        Some(String::from("test2"))
+    );
 }
